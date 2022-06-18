d76784ca35cccb454abe1df1c4506b94ecd2a4ec
Margarida Estrela <margarida.e2002@gmail.com>
2022-06-18 13:35:26 +0100

    unique col&row + verify 0s & 1s

--- takuzu.py
+++ takuzu.py
@@ -173,7 +173,7 @@ class Takuzu(Problem):
 
         if state.free == 0:
             return act
-            
+
         else:
             first_act = self.get_first_mandatory_free_position(state.board)
 
@@ -226,8 +226,10 @@ class Takuzu(Problem):
             board.change_number(pos[0], pos[1], 2)
             if len(act) == 1:
                 return act
-  
+
         return []
+
+
 
     def verify_adjacent_horizontal(self, board: Board, pos, value):
         """ Retorna True caso não haja mais que 2 numeros iguais adjacentes
@@ -256,7 +258,6 @@ class Takuzu(Problem):
         adj_v = (value, ) + board.adjacent_vertical_numbers(row, col)
 
         return len(unique(adj_v)) != 1
-
 
     def verify_adjacent(self, board: Board, pos, value):
         """ Retorna True caso não haja mais que 2 numeros iguais adjacentes
@@ -315,15 +316,31 @@ class Takuzu(Problem):
                 self.verify_adjacent_vertical(board, (row+1, col), down) and \
                 self.verify_adjacent_vertical(board, pos, value)
 
-    def unique_row_col(self, board: Board):
+    def verify_row_col(self, board: Board):
         """ Retorna True se e só se todas as linhas e colunas forem
          diferentes."""
         n = board.size()
         col = []
         row = []
+
+        if n % 2 == 0:
+            limit = n//2
+        else:
+            limit = n//2 + 1
+
         for i in range(n):
-            col += [board.get_col(i), ]
-            row += [board.get_row(i), ]
+            c = board.get_col(i)
+            r = board.get_row(i)
+
+            for value in range(n):
+                value_row = np.count_nonzero(row == value)
+                value_col = np.count_nonzero(col == value)
+
+                if value_row > limit or value_col > limit:
+                    return False
+            
+            col += [c, ]
+            row += [r, ]
 
         return self.find_duplicates(col) and self.find_duplicates(row)
 
@@ -350,6 +367,7 @@ class Takuzu(Problem):
         new_board = Board(n)
         new_board.board_repr = copy.deepcopy(state.board.board_repr)
         new_board.change_number(row, col, value)
+
         new_state = TakuzuState(new_board, state.free - 1)
 
         return new_state
@@ -360,7 +378,7 @@ class Takuzu(Problem):
         estão preenchidas com uma sequência de números adjacentes."""
         # TODO
 
-        return state.free == 0 and self.unique_row_col(state.board)
+        return state.free == 0 and self.verify_row_col(state.board)
 
     def h(self, node: Node):
         """Função heuristica utilizada para a procura A*."""
